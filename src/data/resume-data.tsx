6ce2dd6a82037285ae72b6c5c38b8fe67db7008c
netrebnic <70092629+roblanc@users.noreply.github.com>
2024-01-24 19:38:05 +0200

    main page update

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -22,38 +22,32 @@ import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 export const RESUME_DATA = {
   name: "Bartosz Jarocki",
   initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  location: "Bucharest, Romania, EET",
+  locationLink: "https://www.google.com/maps/place/Bucharest",
   about:
     "This is R.",
   summary:
     "You’ve managed to find your way into my working notes, which I’ve accumulated over the course of several years. This database is part of my endeavor to arrange my own notions and ideas in a more public place where I can link notes on various topics together. These are in a constant state of flux. I also post/experiment with visual trickery on my YouTube channel, ideas and theories that interest me. I use it to alleviate boredom and occasionally heighten my thought processes. I am a systematizer, and this is my pocket dimension. ",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  personalWebsiteUrl: "https://rtheory.xyz",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "romicam2@gmail.com",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/roblanc",
         icon: GitHubIcon,
       },
       {
-        name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
-        icon: LinkedInIcon,
-      },
-      {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/rtheoryxzy",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
+      school: "Faculty of Foreign Languages and Literatures (University of Bucharest",
       degree: "Bachelor's Degree in Control systems engineering and Robotics",
       start: "2007",
       end: "2010",
